22d903384c4eb3ac72198eb714dbdd7674e7d68c
appDeveloper7 <85474707+appDeveloper7@users.noreply.github.com>
2024-11-18 12:40:39 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,8 +19,16 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
+        // Public-facing target
         .target(
             name: "MySwiftPackage"),
-
+        // Hidden implementation target
+        .target(
+            name: "MySwiftPackageImplementation",  // Internal implementation target
+            dependencies: [],
+            path: "Sources/MySwiftPackageImplementation",
+            publicHeadersPath: ""  // No public headers exposed
+        )
+        
     ]
 )
